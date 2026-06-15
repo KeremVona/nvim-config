1ef47aff8ecc21fb1ded11c3e86317f96d47bc6c
KeremVona <keremvona@gmail.com>
2026-06-15 11:30:56 +0300

    import check works

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,24 +1,32 @@
 return {
   {
-    "seblj/roslyn.nvim",
+    "seblyng/roslyn.nvim",
     ft = "cs",
     config = function()
-      require("roslyn").setup({
-        exe = "/home/vona50/.local/share/nvim/roslyn/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer",
-        args = {
+      local has_blink, blink = pcall(require, "blink.cmp")
+      local capabilities = has_blink and blink.get_lsp_capabilities() or nil
+
+      vim.lsp.config("roslyn", {
+        cmd = {
+          "/home/vona50/.local/share/nvim/roslyn/content/LanguageServer/linux-x64/Microsoft.CodeAnalysis.LanguageServer",
           "--logLevel=Information",
           "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.log.get_filename()),
           "--stdio",
         },
-        config = {
-          on_attach = function(client, bufnr) end,
-          settings = {
-            ["csharp|background_analysis"] = {
-              dotnet_analyzer_diagnostics_scope = "fullSolution",
-              dotnet_compiler_diagnostics_scope = "fullSolution",
-            },
+        capabilities = capabilities,
+        settings = {
+          ["csharp|background_analysis"] = {
+            dotnet_analyzer_diagnostics_scope = "fullSolution",
+            dotnet_compiler_diagnostics_scope = "fullSolution",
+          },
+          ["csharp|completion"] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
           },
         },
+      })
+
+      require("roslyn").setup({
         extensions = {
           razor = { enabled = false },
         },
