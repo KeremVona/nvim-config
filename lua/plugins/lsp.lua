3666a0e04aa976c6a0211f875a290260161cd8b2
KeremVona <keremvona@gmail.com>
2026-06-15 11:12:21 +0300

    organize

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,9 +4,30 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
+        -- Disable eslint autostart
         eslint = {
           autostart = false,
           enabled = false,
+        },
+        -- Go language server settings
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              completeUnimported = true,
+            },
+          },
         },
       },
     },
